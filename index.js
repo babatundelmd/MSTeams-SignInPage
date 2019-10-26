eb984c619569f33aa1a925f5117f3933c6cbe533
Babatunde Lamidi <babatundela@ha-shem.com>
2019-10-26 15:52:27 +0100

    feat: email validation works

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const iemail = document.querySelector('#input-email');
+const btn = document.querySelector('#btn');
 
 function klassed() {
-    console.log('works')
     iemail.classList.add('classed');
 }
 
+function isEmailValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
 iemail.addEventListener('click', klassed)
+
+iemail.addEventListener('input', (e) => {
+    if (isEmailValid(e.target.value)) {
+        btn.classList.add('validate');
+    }
+    else {
+        btn.classList.remove('validate')
+    }
+})
