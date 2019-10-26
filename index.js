e068e1e9e54202db6cbb557389dc91258d4166fb
Babatunde Lamidi <babatundela@ha-shem.com>
2019-10-26 16:09:14 +0100

    refactor: eventlistener

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const iemail = document.querySelector('#input-email');
 const btn = document.querySelector('#btn');
 
-function klassed() {
-    iemail.classList.add('classed');
-}
-
 function isEmailValid(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
-iemail.addEventListener('click', klassed)
+
+iemail.addEventListener('click', () => {
+    iemail.classList.add('classed');
+})
 
 iemail.addEventListener('input', (e) => {
     if (isEmailValid(e.target.value)) {
